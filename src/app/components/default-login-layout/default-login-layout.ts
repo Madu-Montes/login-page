7637fea7aeb42c5591a19d3da8584dab8b4f0b16
madu-montes <monteseduarda56@gmail.com>
2026-03-24 21:36:52 -0300

    feat: login page style

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   imports: [],
   templateUrl: './default-login-layout.html',
   styleUrl: './default-login-layout.scss',
+  standalone: true,
 })
-export class DefaultLoginLayout {}
+export class DefaultLoginLayout {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+}
